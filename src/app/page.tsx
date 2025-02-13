4db0d5928e51336b13d3af32a955c5c70819a178
Vlad Stoenescu <64587367+vladstoe@users.noreply.github.com>
2025-02-14 00:39:39 +0100

    fixed bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
-// app/page.tsx
-import { redirect } from 'next/navigation';
+// src/app/page.tsx
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useUser } from "@/context/UserContext";
 
 export default function Index() {
-    redirect('/home'); // Redirects to the login page
+    const router = useRouter();
+    const { user, isLoadingUser } = useUser();
+
+    useEffect(() => {
+        if (!isLoadingUser) {
+            if (user) {
+                router.replace("/home"); // If logged in, go to home
+            } else {
+                router.replace("/login"); // If not, go to login
+            }
+        }
+    }, [user, isLoadingUser, router]);
+
+    return null; // Render nothing during redirect
 }
